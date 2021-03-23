3f2aad7594f118b421908a8b51906a737433eddb
Marta Taulet <mta@odoo.com>
2021-03-23 15:53:12 -0700

    [IMP] Add paperformat

--- quotation_builder_pdf/__manifest__.py
+++ quotation_builder_pdf/__manifest__.py
@@ -7,17 +7,18 @@
         Quotation Builder PDF""",
 
     'description': """
-        Enables the printed PDF of the quotation to display the same modifications
+        Task #2314417 - Enables the printed PDF of the quotation to display the same modifications
         as the portal view. Text and image customizations done with Odoo's Quotation
         Builder will be displayed on the PDF. The animated and complex widgets
-        designed for the webiste will not be displaid on the report.
+        designed for the webiste will not be displayed on the report.
     """,
     'author': "Odoo Inc",
     'website': "http://www.odoo.com",
-    'category': 'Sales',
+    'category': 'Custom Development',
     'version': '0.1',
     'depends': ['sale_quotation_builder', 'website', 'web_enterprise'],
     'data': [
+        'data/report_paperformat.xml',
         'views/assets.xml',
         'reports/sale_report.xml',
     ],
