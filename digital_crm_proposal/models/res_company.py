b94b491083f820b482b0c8130de7bca365be3391
Marta Taulet <mtaulet@bu.edu>
2021-11-29 17:02:05 +0100

    [REM] Remove third party modules

--- digital_crm_proposal/models/res_company.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-
-class Company(models.Model):
-    _inherit = "res.company"
-
-    is_proposal = fields.Boolean("Proposal Available", default=False)
-    attach_terms_conditions = fields.Many2many('ir.attachment', 'terms_attachment_rel', 'company_id', 'attach_id', string="Terms and Conditions")
